bf2b189c882b0a9e5d66e87c903bb9711ae809d5
HP van Braam <hp@tmm.cx>
2026-07-09 15:59:46 +0200

    Downgrade to glibc-2.34
    This matches the glibc shipped with EL9.

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,7 +7,7 @@
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.43-27-g4070d808bea1c077eb7e7d52b52b91cae98205d5
+GLIBC_VERSION = 2.34-527-g2b656ff94d72f93c84d8da2e7c76456c1994f02e
 GLIBC_SITE = https://sourceware.org/git/glibc.git
 GLIBC_SITE_METHOD = git
 
